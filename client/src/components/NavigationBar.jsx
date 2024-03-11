a67aeff45e26ef5fd6089736b1773dbdddff8400
daredevil17052004 <anshs052004@gmail.com>
2024-03-11 09:48:21 +0530

    added the second page in the website

--- client/src/components/NavigationBar.jsx
+++ client/src/components/NavigationBar.jsx
@@ -6,27 +6,10 @@ const NavigationBar = () => {
   return (
     <div className={styles.main}>
         <div className={styles.container}>
-            <div className={styles.logo}>
-                <img src="../../public/instrurentals-high-resolution-logo-black.png" alt="" />
-            </div>
             <div className={styles.navs}>
-                <div>Home</div>
-                <div>Client</div>
-                <div>Studios</div>
-            </div>
-            <div className={styles.links}>
-                <a href="">
-                    <i class="fi fi-brands-instagram"></i>
-                </a>
-                <a href="">
-                    <i class="fi fi-brands-facebook"></i>
-                </a>
-                <a href="">
-                    <i class="fi fi-brands-twitter"></i>
-                </a>
-                <a href="">
-                    <i class="fi fi-brands-youtube"></i>
-                </a>
+                <div className={styles.l}>Home</div>
+                <div className={styles.l}>Client</div>
+                <div className={styles.l}>Studios</div>
             </div>
             <div className={styles.buttons}>
                 <div className={styles.login}>
